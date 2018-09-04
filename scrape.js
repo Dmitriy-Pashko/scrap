682c44f9178414b82b658f34e785139ffd15a17c
Dmitriy Pashko <pahkovoliacable@ukr.net>
2018-09-04 22:51:27 +0300

    repetable jobs

--- scrape.js
+++ scrape.js
@@ -4,43 +4,41 @@ var jobs = require('./server/model/dbmodel');
 var bodyParser = require('body-parser');
 const puppeteer = require('puppeteer');
 
-var app = express();
-var router = express.Router();
+// var app = express();
+// var router = express.Router();
 
-var port = process.env.API_PORT || 3001;
-mongoose.connect('mongodb://god:blessrng1@ds139992.mlab.com:39992/scrapy', { useNewUrlParser: true });
+// var port = process.env.API_PORT || 3001;
+// mongoose.connect('mongodb://god:blessrng1@ds139992.mlab.com:39992/scrapy', { useNewUrlParser: true });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+//     next();
+// });
 
 let scrape = async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
     await page.goto('https://jobs.dou.ua/vacancies/?category=Front+End');
 
-    while (page.$('.more-btn') !== null ) {
+    for(i=0;i<3;i++){
         await page.click('.more-btn > a');
+        await page.waitFor(4000);
     }
-    await page.waitFor(1000);
-    await page.once('load', () => console.log('Page loaded!'));
-    // if(page.$('.more-btn')){
+
+    // await page.waitForSelector('.more-btn')
+    //     .then(() => page.click('.more-btn > a'));
+
+    // while (page.$('.more-btn') !== null ) {
     //     await page.click('.more-btn > a');
-    //     await page.waitFor(1000);
-    // } else {
-    //     await page.once('load', () => console.log('Page loaded!'));
     // }
 
-    //making pagination gone
-    // let btn = document.querySelector('.more-btn').childNodes[0];
-
+    await page.once('load', () => console.log('Page loaded!'));
 
     // Scrape
     const result = await page.evaluate(() => {
@@ -83,8 +81,8 @@ scrape().then((value) => {
     })
 });
 
-app.use('/api', router);
+// app.use('/api', router);
 
-app.listen(port, function() {
-    console.log('derr');
-});
+// app.listen(port, function() {
+//     console.log('derr');
+// });
---
+++ scraping.js
@@ -0,0 +1,76 @@
+const puppeteer = require('puppeteer');
+var mongoose = require('mongoose');
+var jobs = require('./server/model/dbmodel');
+
+mongoose.connect('mongodb://god:blessrng1@ds139992.mlab.com:39992/scrapy', { useNewUrlParser: true });
+
+function run (pagesToScrape) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (!pagesToScrape) {
+                pagesToScrape = 1;
+            }
+
+            console.log(pagesToScrape);
+            pagesToScrape = 2;
+            const browser = await puppeteer.launch({headless: false});
+            const page = await browser.newPage();
+            await page.goto('https://jobs.dou.ua/vacancies/?category=Front+End');
+            
+            //nonsense try for getting jobs count
+            // let cities = document.querySelectorAll('ul.other > li > em');
+            let currentPage = 1;
+            let urls = [];
+            while (currentPage <= pagesToScrape) {
+                let newUrls = await page.evaluate(() => {
+                    let results = [];
+                    let items = document.querySelectorAll('.vacancy');
+
+                    items.forEach((item) => {
+
+                        let link = item.childNodes[1].children[0].href;
+                        let title = item.childNodes[1].children[0].innerText;
+                        let desc = item.childNodes[3].innerText;
+
+                        results.push({
+                            link,
+                            title,
+                            desc
+                        });
+                    });
+                    return results;
+                });
+                urls = urls.concat(newUrls);
+                if (currentPage < pagesToScrape) {
+                    await Promise.all([
+                        await page.click('.more-btn > a'),
+                        await page.waitForSelector('.more-btn'),
+                        await page.waitFor(1000)
+                    ])
+                }
+                currentPage++;
+            }
+            browser.close();
+            return resolve(urls);
+        } catch (e) {
+            return reject(e);
+        }
+    })
+}
+
+// run(3).then(console.log).catch(console.error);
+run().then((value)=>{
+    console.log(value); // Success!
+    value.forEach(function(dou){
+            let job = new jobs({
+                link: dou.link,
+                title: dou.title,
+                description: dou.desc,
+            });
+            job.save(function(err) {
+                if (err)
+                    return console.log(err);
+                console.log('Job successfully added!');
+            });
+    })
+}).catch(console.error);
