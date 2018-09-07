c8c78855b3e4c51083e6eab962742ef980b986fd
Dmitriy Pashko <pahkovoliacable@ukr.net>
2018-09-07 14:18:04 +0300

    now I see

--- promise.js
+++ promise.js
@@ -11,12 +11,8 @@ let scrape = () => {
                     let more = () => {
                         if(page.$('.more-btn a:not([style*="display: none"])') !== null) {
                             page.waitForSelector('.more-btn')
-                                .then(() => {
-                                    return page.waitFor(1000);
-                                })
-                                .then(() => {
-                                    return page.click('.more-btn > a');
-                                })
+                                .then(() => page.waitFor(1000))
+                                .then(() => page.click('.more-btn > a'))
                                 .then(more);
                         }
                         else {
@@ -26,15 +22,6 @@ let scrape = () => {
                     }
                     return more();
                 })
-                // .then(() => {
-                //     return page.waitForSelector('.more-btn');
-                // })
-                // .then(() => {
-                //     return page.waitFor(1000);
-                // })
-                // .then(() => {
-                //     return page.click('.more-btn > a');
-                // })
         })
 
         // browser.close();
