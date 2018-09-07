2c752910e1d14973e48b45962a12ac1ae79bfb6f
Dmitriy Pashko <pahkovoliacable@ukr.net>
2018-09-07 16:11:40 +0300

    scope lost2

--- promise.js
+++ promise.js
@@ -3,43 +3,40 @@ const puppeteer = require('puppeteer');
 let scrape = () => {
     return puppeteer.launch({headless: false})
         .then((browser) => browser.newPage()
-        
+            .then((page) => {
+                return page.goto('https://jobs.dou.ua/vacancies/?category=Front+End')
+                    .then(() => {
+                        let more = () => {
+                            return page.waitFor(1000)
+                                .then(() => page.waitForSelector('.more-btn'))
+                                .then(() => page.$('.more-btn a:not([style*="display: none"])'))
+                                .then((result) => {
+                                    if(result !== null) {
+                                        return page.click('.more-btn > a')
+                                            .then(more);
+                                    }
+                                    else {
+                                        return page.evaluate(()=>{
+                                            let arr = [];
+                                            let elements = page.$$('.vacancy');
+                    
+                                            elements.forEach((elem)=>{
+                                                let link = elem.childNodes[1].children[0].href;
+                                                let title = elem.childNodes[1].children[0].innerText;
+                                                let desc = elem.childNodes[3].innerText;
+                    
+                                                arr.push({link, title, desc});
+                                            })
+                                            return arr;
+                                        }).then((arr) => console.log(arr.length))
+                                        // return console.log("No more buttons");
+                                    }
+                                })
+                        }
+                        return more();
+                    })              
+            }).then(() => browser.close())
         )
-        .then((page) => {
-            return page.goto('https://jobs.dou.ua/vacancies/?category=Front+End')
-                .then(() => {
-                    let more = () => {
-                        return page.waitFor(1000)
-                            .then(() => page.waitForSelector('.more-btn'))
-                            .then(() => page.$('.more-btn a:not([style*="display: none"])'))
-                            .then((result) => {
-                                if(result !== null) {
-                                    return page.click('.more-btn > a')
-                                        .then(more);
-                                }
-                                else {
-                                    // return page.evaluate(()=>{
-                                    //     let arr = [];
-                                    //     let elements = page.$$('.vacancy');
-                
-                                    //     elements.forEach((elem)=>{
-                                    //         let link = elem.childNodes[1].children[0].href;
-                                    //         let title = elem.childNodes[1].children[0].innerText;
-                                    //         let desc = elem.childNodes[3].innerText;
-                
-                                    //         arr.push({link, title, desc});
-                                    //     })
-                                    //     return arr;
-                                    // }).then((arr) => console.log(arr.length))
-                                    return console.log("No more buttons");
-                                }
-                            })
-                    }
-                    return more();
-                })              
-        }).then(() => {
-            browser.close();
-        })
 }
 
 scrape();
