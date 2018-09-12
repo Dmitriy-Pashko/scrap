5679d86154c5a6a5b3cac100132fb41d9e3491e0
Dmitriy Pashko <pahkovoliacable@ukr.net>
2018-09-12 13:18:47 +0300

    refactoring

--- promise.js
+++ promise.js
@@ -1,51 +1,34 @@
 const puppeteer = require('puppeteer');
 const saveData = require('./savedata');
-const Promise = require('bluebird');
 
-let scrape = () => {
-    return puppeteer.launch({headless: false})
+let scrape = () => puppeteer.launch({headless: false})
         .then((browser) => browser.newPage()
-            .then((page) => {
-                return page.goto('https://jobs.dou.ua/vacancies/?category=Front+End')
-                    .then(() => {
-                        let more = () => {
-                            return page.waitFor(1000)
-                                .then(() => page.waitForSelector('.more-btn'))
-                                .then(() => page.$('.more-btn a:not([style*="display: none"])'))
-                                .then((result) => {
-                                    if(result !== null) {
-                                        return page.click('.more-btn > a')
-                                            .then(more);
-                                    }
-                                    return page.evaluate(()=>{
-                                        let arr = [];
-                                        let elements = document.querySelectorAll('.vacancy');
-                                        
-                                        Promise.each(elements, function(elem) {
-                                            return console.log(elem);
-                                            // let link = elem.childNodes[1].children[0].href;
-                                            // let title = elem.childNodes[1].children[0].innerText;
-                                            // let desc = elem.childNodes[3].innerText;
-                
-                                            // arr.push({link, title, desc});
-                                            // return arr;
-                                        })
-                                        // elements.forEach((elem)=>{
-                                        //     let link = elem.childNodes[1].children[0].href;
-                                        //     let title = elem.childNodes[1].children[0].innerText;
-                                        //     let desc = elem.childNodes[3].innerText;
-                
-                                        //     arr.push({link, title, desc});
-                                        // })
-
-                                        return arr;
-                                    }).then((arr) => saveData(arr));
+            .then((page) => page.goto('https://jobs.dou.ua/vacancies/?category=Front+End')
+                .then(() => {
+                    let more = () => page.waitFor(1000)
+                        .then(() => page.waitForSelector('.more-btn'))
+                        .then(() => page.$('.more-btn a:not([style*="display: none"])'))
+                        .then((result) => {
+                            if(result !== null) {
+                                return page.click('.more-btn > a')
+                                    .then(more);
+                            }
+                            return page.evaluate(()=>{
+                                let arr = [];
+                                let elements = document.querySelectorAll('.vacancy');
+        
+                                elements.forEach((elem)=>{
+                                    let link = elem.childNodes[1].children[0].href;
+                                    let title = elem.childNodes[1].children[0].innerText;
+                                    let desc = elem.childNodes[3].innerText;
+        
+                                    arr.push({link, title, desc});
                                 })
-                        }
-                        return more().catch(err => console.log(err));
-                    })              
-            }).then(() => browser.close())
-        )
-}
+                                return arr;
+                            }).then((arr) => saveData(arr));
+                        })
+                    return more().catch(err => console.log(err));
+                }))
+                .then(() => browser.close()))
 
 scrape();
--- savedata.js
+++ savedata.js
@@ -1,12 +1,13 @@
-var mongoose = require('mongoose');
-var jobs = require('./server/model/dbmodel');
+const mongoose = require('mongoose');
+const jobs = require('./server/model/dbmodel');
+const promise = require('bluebird');
 
 mongoose.connect('mongodb://god:blessrng1@ds139992.mlab.com:39992/scrapy', { useNewUrlParser: true });
 
 let saveJobs = (jobsArray) => {
     return new Promise((resolve, reject) => {
-        let arr = jobsArray.forEach((dou) => {
-            let job = new jobs({
+        let arr = promise.each(jobsArray, function(dou){
+            job = new jobs({
                 link: dou.link,
                 title: dou.title,
                 description: dou.desc,
