1b1389f13b048d67a8084f13c0fd683bddc24a3b
Dmitriy Pashko <pahkovoliacable@ukr.net>
2018-09-18 12:21:30 +0300

    Chouse your destiny, flowless victory

--- scraping.js
+++ scraping.js
@@ -1,7 +1,6 @@
 const puppeteer = require('puppeteer');
 const saveData = require('./savedata');
 
-let i = 1;
 let scrape = () => puppeteer.launch({headless: false})
     .then((browser) => browser.newPage()
         .then((page) => page.goto('https://jobs.dou.ua/')
@@ -9,22 +8,33 @@ let scrape = () => puppeteer.launch({headless: false})
                 let chooseCategory = () => page.waitFor(1000)
                     .then(() => page.waitForSelector('select'))
                     .then(() => page.click('select'))
-                    .tnen(() => page.evaluate(() => {
+                    .then(() => page.waitFor(4000))
+                    .then(() => page.evaluate((counter) => {
                         let last = document.querySelector('option:last-child');
                         let selectedOption = document.querySelector('option[selected]');
 
                         if(last != selectedOption){
-                            let select = document.querySelector('selected');
-                            select.selectedIndex = i;
-                            i++;
-                            console.log(selectedOption);
-                            return chooseCategory;
+                            let select = document.querySelector('select');
+                            select.selectedIndex = counter;
+                            counter++;
+                            select.parentNode.submit();
+                            return {
+                                counter
+                            }
+                        } else {
+                            return null;
+                        }
+                    }, 1))
+                    .then((counter) => {
+                        if (counter) {
+                            return chooseCategory()
                         } else {
                             return true;
                         }
-                    }))
-                return chooseCategory()
+                    })
+                        
+                return chooseCategory();
             })
         ).then(() => browser.close())
-    )
+    ).catch((err) => console.log(err));
 scrape();
