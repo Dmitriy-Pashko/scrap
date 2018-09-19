536c440681f85fe73404952949ad3d86ac758496
Dmitriy Pashko <pahkovoliacable@ukr.net>
2018-09-19 13:07:10 +0300

    else if

--- scraping.js
+++ scraping.js
@@ -4,18 +4,26 @@ const saveData = require('./savedata');
 let mycounter = 1;
 let scrape = () => puppeteer.launch({headless: false})
     .then((browser) => browser.newPage()
-        .then((page) => page.goto('https://jobs.dou.ua/')
+        .then((page) => page.goto('https://jobs.dou.ua')
             .then(() => {
                 let chooseCategory = () => page.waitFor(1000)
                     .then(() => page.waitForSelector('select'))
                     .then(() => page.click('select'))
                     .then(() => page.waitFor(4000))
                     .then(() => page.evaluate((counter) => {
-                        let last = document.querySelector('option:last-child').text;
-                        let selectedOption = document.querySelector('option[selected]').text;
 
-                        if(last != selectedOption){
-                            let select = document.querySelector('select');
+                        let last = document.querySelector('option:last-child').text;
+                        let selectedOption = document.querySelector('option[selected]');
+                        let select = document.querySelector('select');
+
+                        if (selectedOption === null) {
+                            select.selectedIndex = counter;
+                            counter++;
+                            select.parentNode.submit();
+                            return {
+                                counter
+                            }
+                        } else if (last != selectedOption.text) {
                             select.selectedIndex = counter;
                             counter++;
                             select.parentNode.submit();
@@ -25,6 +33,7 @@ let scrape = () => puppeteer.launch({headless: false})
                         } else {
                             return null;
                         }
+                        
                     }, mycounter))
                     .then((counter) => {
                         mycounter = counter;
